e872a8005f0fddc1fc05f66a2eaae422942694a6
tukimoto <74166683+tukimoto@users.noreply.github.com>
2021-02-03 09:06:44 +0900

    更新

--- src/main/java/com/example/demo/MainContoroller.java
+++ src/main/java/com/example/demo/MainContoroller.java
@@ -3,17 +3,23 @@ package com.example.demo;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -23,6 +29,9 @@ public class MainContoroller {
 	
 	@Autowired
 	DiaryRepository  diaryrepository;
+	
+	@Autowired
+	HttpSession session;
 	
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public ModelAndView indexGet(ModelAndView mv) {
@@ -67,7 +76,35 @@ public class MainContoroller {
 			
 		return new ModelAndView("redirect:/mypage/"+id);
 	}
+	private final String VIEW_NAME="index";
+	@GetMapping("cookie0")
+	public String index(HttpServletResponse httpServletResponse) {
+		httpServletResponse.addCookie(new Cookie("key1","hello"));
+		return VIEW_NAME;
+	}
+	@GetMapping("cookies1")
+	public String showCookies1(@CookieValue("key1")String cookieValue) {
+		System.out.println(cookieValue);
+		return VIEW_NAME;
+	}
+	@GetMapping("cookies2")
+	public String showCookies2(@CookieValue(name="key1",required=false,defaultValue="default value1")String cookieValue) {
+		System.out.println(cookieValue);
+		return VIEW_NAME;
+	}
 	
+	@RequestMapping(value="/seve",method=RequestMethod.GET)
+	public String save() {
+		session.setAttribute("data", "保存したいデータ");
+		return "index";
+	}
+	@RequestMapping(value="/load",method=RequestMethod.GET)
+	public String load() {
+		String data =(String) session.getAttribute("data");
+		System.out.print(data);
+		session.invalidate();
+		return "index";
+	}
 		
 	
 	
